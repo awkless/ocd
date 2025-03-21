8eae93897786c58870c78f18a364d24029935994
Jason Pena <jasonpena@awkless.com>
2025-03-21 01:40:29 -0600

    feat(cluster): add `Cluster::get_root`
    Allow caller to get root of cluster in deserialized form. This method
    uses the newly implemented `Root::from` method to obtain the
    deserialized `Root` type from a `&Table` type (root table of TOML
    document).
    
    The `Root::from` method works by implementing
    `Visit::visit_table_like_kv` trait method to visit each node of the
    given `&Table` parameter. The `VisitState` enum is used to ensure that
    only the key-value pairs in the root table are extracted, ignoring any
    other key-value pairs nested in tables that use the same "worktree"
    and "excludes" pairs.
    
    Tests:
    
    1. Ensure that `Cluster::get_root` extracts "worktree" and "excludes"
       key-value pair at root table only. It should ignore any other
       key-value pair of the same name as "worktree" or "excludes" nested
       inside of other tables besides the root table.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/cluster.rs
+++ src/cluster.rs
@@ -1,9 +1,12 @@
 // SPDX-FileCopyrightText: 2025 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT or Apache-2.0
 
-use anyhow::{anyhow, Context, Result};
-use toml_edit::DocumentMut;
+use anyhow::{Context, Result};
 use std::path::PathBuf;
+use toml_edit::{
+    visit::{visit_table_like_kv, Visit},
+    DocumentMut, Item, Table,
+};
 
 #[derive(Default, Debug)]
 pub struct Cluster {
@@ -11,9 +14,15 @@ pub struct Cluster {
 }
 
 impl Cluster {
-    // TODO: Implement this.
-}
+    pub fn new() -> Self {
+        Cluster::default()
+    }
 
+    pub fn get_root(&self) -> Root {
+        let table = self.document.as_table();
+        Root::from(table)
+    }
+}
 impl std::str::FromStr for Cluster {
     type Err = anyhow::Error;
 
@@ -33,10 +42,86 @@ impl std::fmt::Display for Cluster {
 pub struct Root {
     pub worktree: Option<PathBuf>,
     pub excludes: Option<Vec<String>>,
+    state: VisitState,
 }
 
 impl Root {
     pub fn new() -> Self {
-        Default::default()
+        Root::default()
+    }
+}
+
+impl<'toml> From<&'toml Table> for Root {
+    fn from(table: &'toml Table) -> Self {
+        let mut root = Root { ..Default::default() };
+        root.visit_table(table);
+        root
+    }
+}
+
+impl<'toml> Visit<'toml> for Root {
+    fn visit_table_like_kv(&mut self, key: &'toml str, node: &'toml Item) {
+        if self.state != VisitState::Root {
+            self.state = self.state.descend(key);
+            visit_table_like_kv(self, key, node);
+        }
+
+        if key == "worktree" {
+            self.worktree = node.as_str().map(Into::into);
+        }
+
+        if key == "excludes" {
+            self.excludes = node
+                .as_array()
+                .map(|a| a.into_iter().map(|s| s.as_str().unwrap_or_default().into()).collect());
+        }
+    }
+}
+
+#[derive(Copy, Default, Clone, Debug, Eq, PartialEq)]
+enum VisitState {
+    #[default]
+    Root,
+    Other,
+}
+
+impl VisitState {
+    fn descend(self, key: &str) -> Self {
+        match (self, key) {
+            (VisitState::Root, "worktree" | "excludes") => VisitState::Root,
+            (VisitState::Root | VisitState::Other, _) => VisitState::Other,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn smoke_cluster_get_root() -> Result<()> {
+        let toml = r#"
+            worktree = "/some/path"
+            excludes = ["file1", "file2"]
+
+            [foo]
+            worktree = "/blah/blah"
+            excludes = ["ignore1", "ignore2"]
+
+            [node.vim]
+            worktree = ".vimrc"
+            excludes = ["ftplugin", "*.bak"]
+        "#;
+
+        let cluster: Cluster = toml.parse()?;
+        let result = cluster.get_root();
+        let expect = Root {
+            worktree: Some("/some/path".into()),
+            excludes: Some(vec!["file1".into(), "file2".into()]),
+            state: VisitState::default(),
+        };
+        assert_eq!(result, expect);
+
+        Ok(())
     }
 }
