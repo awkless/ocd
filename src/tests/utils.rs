e0e5647107f592c6ade64348159471b781106559
Jason Pena <jasonpena@awkless.com>
2025-04-03 21:58:52 -0600

    test: simplify `smoke_glob_match` through `rstest`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/tests/utils.rs
+++ src/tests/utils.rs
@@ -5,24 +5,21 @@
 use crate::utils::glob_match;
 
 use pretty_assertions::assert_eq;
+use rstest::rstest;
 
-#[track_caller]
-fn check_glob_match(
-    patterns: impl IntoIterator<Item = impl Into<String>>,
-    entries: impl IntoIterator<Item = impl Into<String>>,
-    expect: impl IntoIterator<Item = impl Into<String>>,
+#[rstest]
+#[case::match_all(["*"], ["foo", "bar", "baz"], ["foo", "bar", "baz"])]
+#[case::match_single_glob(["*sh"], ["sh", "bash", "yash", "vim"], ["sh", "bash", "yash"])]
+#[case::match_no_glob(["vim", "foo"], ["foo", "dwm", "bar", "vim"], ["vim", "foo"])]
+#[case::no_match(["foo", "bar"], ["vim", "dwm", "sh"], Vec::<String>::new())]
+fn smoke_glob_match(
+    #[case] patterns: impl IntoIterator<Item = impl Into<String>>,
+    #[case] entries: impl IntoIterator<Item = impl Into<String>>,
+    #[case] expect: impl IntoIterator<Item = impl Into<String>>,
 ) {
     let mut expect = expect.into_iter().map(Into::into).collect::<Vec<String>>();
     let mut result = glob_match(patterns, entries);
     expect.sort();
     result.sort();
     assert_eq!(result, expect);
-}
-
-#[test]
-fn smoke_glob_match() {
-    check_glob_match(["*"], ["foo", "bar", "baz"], ["foo", "bar", "baz"]);
-    check_glob_match(["*sh"], ["sh", "bash", "yash", "vim"], ["sh", "bash", "yash"]);
-    check_glob_match(["vim", "foo"], ["foo", "dwm", "bar", "vim"], ["vim", "foo"]);
-    check_glob_match(["foo", "bar"], ["vim", "dwm", "sh"], Vec::<String>::new());
 }
