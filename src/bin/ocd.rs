7b811e6b53deff96dc4da6bc849f54970c5a24aa
Jason Pena <jasonpena@awkless.com>
2025-04-21 20:58:46 -0600

    bin: Cleanup `main`
    Convert `main` from sync to async using `tokio` crate. Also setup `run`
    function to perform the core logic of running the application. The
    `main` function also sets up the tracing subscriber to read logging
    settings from the environment, with the default level being info.
    The `main` function also issues valid exit status on error and success.
    
    The `run` function is empty, but as development progresses, I will fill
    it out.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/bin/ocd.rs
+++ src/bin/ocd.rs
@@ -1,6 +1,25 @@
 // SPDX-FileCopyrightText: 2025 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
-fn main() {
-    println!("Hello from OCD!");
+use ocd::exit_status_from_error;
+
+use anyhow::Result;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+#[tokio::main]
+async fn main() {
+    let format = fmt::layer().pretty();
+    let filter = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info")).unwrap();
+    tracing_subscriber::registry().with(filter).with(format).init();
+
+    if let Err(error) = run().await {
+        tracing::error!("{error:?}");
+        std::process::exit(exit_status_from_error(error));
+    }
+
+    std::process::exit(exitcode::OK);
+}
+
+async fn run() -> Result<()> {
+    todo!();
 }
