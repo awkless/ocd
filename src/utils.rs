8cb3a2b5084508e99f6c24a90b9dc539175de715
Jason Pena <jasonpena@awkless.com>
2025-04-23 01:08:08 -0600

    utils: Add `glob_match`
    Caller can now use unix-like pattern matching to match various strings
    as the they see fit.
    
    Test plan:
    
    - Smoke test `glob_match` for the following test cases:
      1. Works for a valid set of patterns.
      2. Works when no pattern matches.
      3. Works when given invalid patterns.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/utils.rs
+++ src/utils.rs
@@ -10,6 +10,51 @@
 use crate::{Error, Result};
 
 use std::{ffi::OsStr, process::Command};
+use tracing::{instrument, warn};
+
+/// Use Unix-like glob pattern matching.
+///
+/// Will match a set of patterns to a given set of entries. Whatever is matched is returned as a
+/// new vector to operate with. Invalid patterns or patterns with no matches or excluded from the
+/// new vector, and logged as errors.
+///
+/// # Invariants
+///
+/// - Always produce valid vector containing matched entries only.
+/// - Process full pattern list without failing.
+#[instrument(skip(patterns, entries))]
+pub fn glob_match(
+    patterns: impl IntoIterator<Item = impl Into<String>> + std::fmt::Debug,
+    entries: impl IntoIterator<Item = impl Into<String>> + std::fmt::Debug,
+) -> Vec<String> {
+    let patterns = patterns.into_iter().map(Into::into).collect::<Vec<String>>();
+    let entries = entries.into_iter().map(Into::into).collect::<Vec<String>>();
+
+    let mut matched = Vec::new();
+    for pattern in &patterns {
+        let pattern = match glob::Pattern::new(pattern) {
+            Ok(pattern) => pattern,
+            Err(error) => {
+                warn!("Invalid pattern {pattern}: {error}");
+                continue;
+            }
+        };
+
+        let mut found = false;
+        for entry in &entries {
+            if pattern.matches(entry) {
+                found = true;
+                matched.push(entry.to_string());
+            }
+        }
+
+        if !found {
+            warn!("Pattern {} does not match any entries", pattern.as_str());
+        }
+    }
+
+    matched
+}
 
 /// Call external shell program non-interactively.
 ///
@@ -93,6 +138,37 @@ mod tests {
     use super::*;
 
     use simple_test_case::test_case;
+
+    #[test_case(
+        vec!["*sh".into(), "[f-g]oo".into(), "d?o".into()],
+        vec!["sh".into(), "bash".into(), "foo".into(), "goo".into(), "doo".into()],
+        vec!["sh".into(), "bash".into(), "foo".into(), "goo".into(), "doo".into()];
+        "match all"
+    )]
+    #[test_case(
+        vec!["foo".into(), "bar".into()],
+        vec!["vim".into(), "dwm".into(), "sh".into()],
+        Vec::<String>::new();
+        "no match"
+    )]
+    #[test_case(
+        vec!["[1-".into(), "[!a-d".into()],
+        vec!["vim".into(), "dwm".into(), "sh".into()],
+        Vec::<String>::new();
+        "invalid pattern"
+    )]
+    #[test]
+    fn smoke_glob_match(
+        patterns: Vec<String>,
+        entries: Vec<String>,
+        expect: Vec<String>,
+    ) {
+        let mut expect = expect.into_iter().map(Into::into).collect::<Vec<String>>();
+        let mut result = glob_match(patterns, entries);
+        expect.sort();
+        result.sort();
+        pretty_assertions::assert_eq!(result, expect);
+    }
 
     #[test_case(
         "git",
