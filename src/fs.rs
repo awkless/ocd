1b7671d5ab91ca13fea304c08e50db8b0124aad9
Jason Pena <jasonpena@awkless.com>
2025-04-24 18:43:35 -0600

    fs: Add `write_to_config`
    Caller can now write configuration data to external file in serialized
    form through `write_to_config`.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/fs.rs
+++ src/fs.rs
@@ -8,8 +8,16 @@
 
 use crate::{Error, Result};
 
-use std::{fs::read_to_string, path::Path};
+use std::{fs::{OpenOptions, read_to_string}, io::Write, path::Path};
 
+/// Read configuration file and deserialize to target type.
+///
+/// Ignore missing configuration file.
+///
+/// # Errors
+///
+/// - Return `Error::Io` if file cannot be read.
+/// - Return corresponding `Error` variant if deserialization to configuration type fails.
 pub fn read_to_config<C>(path: impl AsRef<Path>) -> Result<C>
 where
     C: std::str::FromStr<Err = Error>,
@@ -21,4 +29,26 @@ where
     }?;
 
     data.parse::<C>()
+}
+
+/// Serialize and write contents of configuration type to target file.
+///
+/// Will create the configuration file to write to, if it does not already exist. Overwrites
+/// original content of target file.
+///
+/// # Errors
+///
+/// - Return `Error::Io` if file cannot be created or written to.
+pub fn write_to_config<C, P>(path: P, config: C) -> Result<()>
+where
+    C: std::fmt::Display,
+    P: AsRef<Path>,
+{
+    OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(path.as_ref())?
+        .write_all(config.to_string().as_bytes())
+        .map_err(Error::from)
 }
