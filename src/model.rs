1235983fcd84c9b6c25f51bd7666aea47a29c6eb
Jason Pena <jasonpena@awkless.com>
2025-05-11 23:17:53 -0600

    model: Rewrite `NodeEntry`
    Similar to the rewrite for `RootEntry`. However still keeps around the
    ability for the user to define the deployment field as either a string
    or inline table mapping, e.g.,
    
    ```
    deployment = "bare_alias"
    ```
    
    or
    
    ```
    deployment = { kind = "bare_alias", work_dir_alias = "some/path" }
    ```
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/model.rs
+++ src/model.rs
@@ -7,8 +7,11 @@
 //! OCD tool.
 
 use anyhow::{anyhow, Result};
-use serde::{Deserialize, Deserializer};
-use std::path::PathBuf;
+use serde::{
+    de::{MapAccess, Visitor},
+    Deserialize, Deserializer,
+};
+use std::{fmt, marker::PhantomData, path::PathBuf, str::FromStr};
 
 /// Root entry of cluster definition.
 ///
@@ -41,7 +44,7 @@ pub struct RootEntry {
 
 fn deserialize_root_work_dir_alias<'de, D>(deserializer: D) -> Result<WorkDirAlias, D::Error>
 where
-    D: Deserializer<'de, Error: >,
+    D: Deserializer<'de>,
 {
     let result: String = Deserialize::deserialize(deserializer)?;
     match result.as_str() {
@@ -51,8 +54,111 @@ where
     }
 }
 
-/// Working directory alias path.
+/// Node entry of cluster.
+///
+/// A cluster typically contains a series of nodes. A given node entry can either be normal or
+/// bare-alias. If the user does not specify a working directory alias, then their home directory
+/// will be used as the default. All nodes contain a URL that points to a remote repository. This
+/// URL is mainly used to tell OCD where to clone the node itself if it is missing in the
+/// repository store.
+///
+/// Node entries have access to the file exclusion feature, and dependency deployment feature.
+/// Thus, each node can have a listing of sparsity rules to exclude files and directories from
+/// deployment, and a listing of other nodes as dependencies that must be deployed with the node
+/// itself.
 #[derive(Debug, PartialEq, Eq, Deserialize)]
+pub struct NodeEntry {
+    /// Deployment method for node entry.
+    #[serde(deserialize_with = "deserialize_node_deployment")]
+    pub deployment: NodeDeployment,
+
+    /// URL to clone node entry from.
+    pub url: String,
+
+    /// List of sparisty rules to exclude files from deployment.
+    pub excluded: Option<Vec<String>>,
+
+    /// List of other nodes to be deployed as dependencies with this node entry.
+    pub dependencies: Option<Vec<String>>,
+}
+
+/// Node deployment method.
+///
+/// Currently, there are only two kinds of node deployment:
+///
+/// 1. Normal deployment kind.
+/// 2. Bare-alias deployment kind.
+///
+/// Normal deployment simply ensures that the node entry has been cloned into repository store.
+/// Bare-alias deployment not only ensures that node entry has been cloned into repository store,
+/// but is also properly deployed to target working directory alias.
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+pub struct NodeDeployment {
+    /// Deployment kind.
+    pub kind: DeploymentKind,
+
+    /// Working directory alias to use.
+    pub work_dir_alias: WorkDirAlias,
+}
+
+impl FromStr for NodeDeployment {
+    type Err = anyhow::Error;
+
+    fn from_str(data: &str) -> Result<Self, Self::Err> {
+        let (kind, work_dir_alias) = match data {
+            "normal" => (DeploymentKind::Normal, WorkDirAlias::default()),
+            "bare_alias" => (DeploymentKind::BareAlias, WorkDirAlias::new(home_dir()?)),
+            _ => return Err(anyhow!("Invalid deployment kind")),
+        };
+
+        Ok(NodeDeployment { kind, work_dir_alias })
+    }
+}
+
+struct NodeDeploymentVisitor(PhantomData<fn() -> NodeDeployment>);
+
+impl<'de> Visitor<'de> for NodeDeploymentVisitor {
+    type Value = NodeDeployment;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("string or map")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<NodeDeployment, E>
+    where
+        E: serde::de::Error,
+    {
+        FromStr::from_str(value).map_err(serde::de::Error::custom)
+    }
+
+    fn visit_map<M>(self, map: M) -> Result<NodeDeployment, M::Error>
+    where
+        M: MapAccess<'de>,
+    {
+        Deserialize::deserialize(serde::de::value::MapAccessDeserializer::new(map))
+    }
+}
+
+fn deserialize_node_deployment<'de, D>(deserializer: D) -> Result<NodeDeployment, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    deserializer.deserialize_any(NodeDeploymentVisitor(PhantomData))
+}
+
+/// Variants of node deployment.
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "snake_case")]
+pub enum DeploymentKind {
+    /// Node is normal, so make sure it got cloned.
+    Normal,
+
+    /// Node is bare-alias, make sure it got cloned, and is deployed to working directory alias.
+    BareAlias,
+}
+
+/// Working directory alias path.
+#[derive(Debug, Default, PartialEq, Eq, Deserialize)]
 pub struct WorkDirAlias(pub(crate) PathBuf);
 
 impl WorkDirAlias {
@@ -106,6 +212,96 @@ mod tests {
     fn root_entry_invalid_work_dir_alias() {
         let result: Result<RootEntry> =
             toml::de::from_str(r#"work_dir_alias = "data_dir""#).with_context(|| "should fail!");
+        assert!(result.is_err());
+    }
+
+    #[test_case(
+        r#"
+            deployment = "normal"
+            url = "https://some/url"
+        "#,
+        NodeEntry  {
+            deployment: NodeDeployment {
+                kind: DeploymentKind::Normal,
+                work_dir_alias: WorkDirAlias::default(),
+            },
+            url: "https://some/url".into(),
+            excluded: None,
+            dependencies: None
+        };
+        "str_normal"
+    )]
+    #[test_case(
+        r#"
+            deployment = "bare_alias"
+            url = "https://some/url"
+        "#,
+        NodeEntry  {
+            deployment: NodeDeployment {
+                kind: DeploymentKind::BareAlias,
+                work_dir_alias: WorkDirAlias::new("some/path"),
+            },
+            url: "https://some/url".into(),
+            excluded: None,
+            dependencies: None
+        };
+        "str_bare_alias"
+    )]
+    #[test_case(
+        r#"
+            deployment = { kind = "normal", work_dir_alias = "blah/blah" }
+            url = "https://some/url"
+        "#,
+        NodeEntry  {
+            deployment: NodeDeployment {
+                kind: DeploymentKind::Normal,
+                work_dir_alias: WorkDirAlias::new("blah/blah"),
+            },
+            url: "https://some/url".into(),
+            excluded: None,
+            dependencies: None
+        };
+        "map_normal"
+    )]
+    #[test_case(
+        r#"
+            deployment = { kind = "bare_alias", work_dir_alias = "blah/blah" }
+            url = "https://some/url"
+        "#,
+        NodeEntry  {
+            deployment: NodeDeployment {
+                kind: DeploymentKind::BareAlias,
+                work_dir_alias: WorkDirAlias::new("blah/blah"),
+            },
+            url: "https://some/url".into(),
+            excluded: None,
+            dependencies: None
+        };
+        "map_bare_alias"
+    )]
+    #[sealed_test(env = [("HOME", "some/path"), ("XDG_CONFIG_HOME", "some/path/.config")])]
+    fn node_entry_valid_deployment(config: &str, expect: NodeEntry) -> Result<()> {
+        let node: NodeEntry = toml::de::from_str(config)?;
+        pretty_assert_eq!(node, expect);
+        Ok(())
+    }
+
+    #[test_case(
+        r#"
+            deployment = "snafu"
+            url = "https://some/url"
+        "#;
+        "invalid_str"
+    )]
+    #[test_case(
+        r#"
+            deployment = { kind = "snafu", work_dir_alias = "blah/blah" }
+            url = "https://some/url"
+        "#;
+        "unknown_field"
+    )]
+    fn node_entry_invalid_deployment(config: &str) {
+        let result: Result<NodeEntry> = toml::de::from_str(config).with_context(|| "should fail!");
         assert!(result.is_err());
     }
 }
