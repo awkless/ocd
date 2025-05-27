e3ed69e1330574d9f19b8a372f18deed5fa3395a
Jason Pena <jasonpena@awkless.com>
2025-05-27 17:57:23 -0600

    bin: Connect command set to binary
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/bin/ocd.rs
+++ src/bin/ocd.rs
@@ -1,8 +1,10 @@
 // SPDX-FileCopyrightText: 2025 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
-// use ocd::{exit_status_from_error, Ocd};
+use ocd::cmd::Ocd;
 
+use anyhow::Result;
+use clap::Parser;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
@@ -11,13 +13,14 @@ async fn main() {
     let filter = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info")).unwrap();
     tracing_subscriber::registry().with(filter).with(layer).init();
 
-    // if let Err(error) = run().await {
-    //     tracing::error!("{error:?}");
-    //     std::process::exit(exit_status_from_error(error));
-    // }
+    if let Err(error) = run().await {
+        tracing::error!("{error:?}");
+        std::process::exit(1);
+    }
+
+    std::process::exit(0);
 }
 
-// async fn run() -> Result<()> {
-//     let ocd = Ocd::parse();
-//     ocd.run().await.with_context(|| "Command failure")
-// }
+async fn run() -> Result<()> {
+    Ocd::parse().run().await
+}
